3e6356c70cfabbb7abb25a295255cc6ce44b870c
Weihong Guan <weihong.guan@gmail.com>
2012-05-31 10:19:12 +0800

    add PCD8544 driver with dotMatrix'

--- BasicDriver/Driver_HD44780_WithoutDotMatrix.h
+++ BasicDriver/Driver_HD44780_WithoutDotMatrix.h
@@ -1,17 +1,17 @@
 /*
-	Driver_HD44780.h
-	Driver_HD44780 Class for dot-matrix printing on liquid crystal display driven by HD47780, namely 1602/1604
-	Created on: 2012-02-29
-	Updated on: 2012-05-14
-
-	library for Arduino for Dot Matrix Display, support driver by 74HC595 and 74HC138, ST7920, HD47780
-	Author: Weihong Guan
-	Blog: http://aguegu.net
-	E-mail: weihong.guan@gmail.com
-	Code license: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
-	http://creativecommons.org/licenses/by-nc-sa/3.0/
-
-	source host: https://github.com/aguegu/dot-matrix
+ *	Driver_HD44780_WithoutDotMatrix.h
+ *	Driver_HD44780 Class for dot-matrix printing on liquid crystal display driven by HD47780, namely 1602/1604
+ *	Created on: 2012-02-29
+ *	Updated on: 2012-05-14
+ *
+ *	library for Arduino for Dot Matrix Display, support driver by 74HC595 and 74HC138, ST7920, HD47780
+ *	Author: Weihong Guan
+ *	Blog: http://aguegu.net
+ *	E-mail: weihong.guan@gmail.com
+ *
+ *	Code license: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
+ *	http://creativecommons.org/licenses/by-nc-sa/3.0/
+ *	source host: https://github.com/aguegu/dot-matrix
  */
 
 #ifndef DRIVER_HD44780_WITHOUT_DOTMATRIX_H_
