34e51e938727b5fc5e498a380f8b0a3a29dd314e
Weihong Guan <weihong.guan@gmail.com>
2012-06-17 23:38:15 +0800

    add animation for 3D8

--- DotMatrixTest.cpp
+++ DotMatrixTest.cpp
@@ -9,21 +9,60 @@ void setup()
 {
 	TCCR2B = (TCCR2B & 0xf8) | 0x02;
 
-	dm.clear(0xff);
+	dm.clear(0x00);
 	//dm.DotMatrix::setDot(0, 0, true);
 	cube.setBrightness(0xff);
-	cube.setMode(0x02);
+	//cube.setMode(0x02);
+	/*
+	 for(byte i=0; i<8; i++)
+	 dm.setDot(i, i, 0, true);
+	 */
 
-	for(byte i=0; i<8; i++)
-		dm.setDot(i, i, 0, true);
+}
+
+void animation_facet_scan(byte speed = 0x04)
+{
+	for (byte j = 0; j < 3; j++)
+	{
+		cube.setMode(j);
+		byte i = 0;
+		while (i < 8)
+		{
+			dm.clear(0x00);
+			dm.setRect(i * 8, 0, i * 8 + 7, 7);
+			cube.display(speed);
+			i++;
+		}
+		while (i)
+		{
+			i--;
+			dm.clear(0x00);
+			dm.setRect(i * 8, 0, i * 8 + 7, 7);
+			cube.display(speed);
+		}
+	}
+}
+
+void animation_flow(word length = 0x40, byte speed = 0x04)
+{
+	word counter;
+	for (byte j = 0; j < 3; j++)
+	{
+		cube.setMode(j);
+		counter = length;
+		while (counter--)
+		{
+			for(byte i=0; i< random(4); i++)
+				dm.setDot(random(8), random(8), 7, true);
+			cube.display(speed);
+			dm.move(DotMatrix::Up, false);
+		}
+	}
 }
 
 void loop()
 {
 
-	dm.setDot(random(8), random(8), 7, true);
-
-	cube.display(0x04);
-	dm.move(DotMatrix::Up, false);
-
+	animation_facet_scan();
+	animation_flow();
 }
