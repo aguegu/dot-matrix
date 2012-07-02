d82fce8e1998e318ebf2a50c0a2f041e992558d8
Weihong Guan <weihong.guan@gmail.com>
2012-07-02 13:03:18 +0800

    begin adjust horizental fonts

--- DotMatrixTest.cpp
+++ DotMatrixTest.cpp
@@ -1,15 +1,13 @@
 #include "DotMatrixTest.h"
 #include "DotMatrix.h"
-#include "DotFont.h"
-#include "DotString.h"
+#include "DotPattern.h"
 #include "Driver_PCD8544.h"
 
-#include "Font0703.h"
-
+#include "Pattern.h"
 
 DotMatrix dm(48, 84);
 Driver_PCD8544 lcd(dm, 2, 3, 4, 5, 6);
-
+DotPattern dp(dm);
 
 void setup()
 {
@@ -19,39 +17,22 @@ void setup()
 	lcd.init();
 
 	dm.clear();
-	dm.setMoveDirection(DotMatrix::BIT_IN_COL_POSI);
+	dm.setMoveDirection(DotMatrix::BIT_IN_COL_NEGA);
+	dp.setPattern(PATTERN_CHAR,72,3);
+	dp.postAt(0, 0);
+	dp.print();
 
-	DotFont df(dm);
-
-	df.setPattern(FONT_0703, FONT_0703_STATE);
-	DotString ds(df, 32, true);
-
-	dm.setDot(0,0);
-
-	ds.printf("Hello, world.");
-	ds.postAt(2,0);
-
-	df.setVertical(false);
-	ds.printf("Font Display on Nokia");
-	ds.postAt(0, 0);
-
-	ds.printf("5110, driven by");
-	ds.postAt(0, 9);
-
-	ds.printf("Arduino.");
-	ds.postAt(0, 18);
-
-	ds.printf("aGuegu.net");
-	ds.postAt(0, 27);
-
-	ds.printf("%2d-%02d, %d", 5, 31, 2012);
-	ds.postAt(0, 36);
+	dp.setPattern(PATTERN_CHAR+72,72,3);
+		dp.postAt(8, 42);
+		dp.print();
 }
 
 void loop()
 {
 	lcd.putDM();
-
-	dm.move(true);
+//	dm.setMoveDirection(DotMatrix::BIT_IN_COL_NEGA);
+//	dm.move(true);
+//	dm.setMoveDirection(DotMatrix::BIT_IN_ROW_NEGA);
+//	dm.move(true);
 	delay(200);
 }
