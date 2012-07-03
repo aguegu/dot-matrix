f2c249c577b85779e45f5935493a734063fcbae8
Weihong Guan <weihong.guan@gmail.com>
2012-07-03 16:31:18 +0800

    add controller_A3D8 to basic driver

--- DotMatrixTest.cpp
+++ DotMatrixTest.cpp
@@ -1,38 +1,100 @@
 #include "DotMatrixTest.h"
-#include "DotMatrix.h"
-#include "DotPattern.h"
-#include "Driver_PCD8544.h"
+#include "Controller_A3D8.h"
 
-#include "Pattern.h"
+extern HardwareSerial Serial;
 
-DotMatrix dm(48, 84);
-Driver_PCD8544 lcd(dm, 2, 3, 4, 5, 6);
-DotPattern dp(dm);
+const byte length = 64;
+byte * pScreen;
+
+Controller_A3D8 cube(Serial);
 
 void setup()
 {
-	pinMode(7, OUTPUT);
-	digitalWrite(7, HIGH);
+	//pScreen = (byte *) malloc(sizeof(byte) * length);
+	pScreen = new byte(length);
+	Serial.begin(57600);
+	cube.sendMode(Controller_A3D8::XYZ);
+	cube.sendBrightness(0xff);
+}
 
-	lcd.init();
+void animationFlash()
+{
+	static byte value = 0xff;
+	cube.sendGlobal(value);
+	value = ~value;
+}
 
-	dm.clear();
-	dm.setMoveDirection(DotMatrix::BIT_IN_COL_NEGA);
-	dp.setPattern(PATTERN_CHAR,72,3);
-	dp.postAt(0, 0);
-	dp.print();
+void animationBreathe()
+{
+	static byte brightness = 0xff;
+	static bool increase = false;
 
-	dp.setPattern(PATTERN_CHAR+72,72,3);
-		dp.postAt(8, 42);
-		dp.print();
+	cube.sendBrightness(increase ? ++brightness : --brightness);
+
+	if (brightness == 0xff)
+		increase = false;
+
+	if (brightness == 0x00)
+		increase = true;
+}
+
+void animationFacetScan()
+{
+	static byte value = 0x01;
+	cube.sendGlobal(value);
+	value <<= 1;
+	if (value == 0x00)
+		value = 0x01;
+}
+
+void animationBlockScan()
+{
+	static byte value = 0x00;
+	value = (value << 1) + 0x01;
+	cube.sendGlobal(value);
+	if (value == 0xff)
+		value = 0x00;
+}
+void animationRiseZ()
+{
+	for (byte i = 0; i < length; i++)
+		pScreen[i] <<= 1;
+
+	for (byte i = 0; i < random(4); i++)
+	{
+		byte x = random(8);
+		byte y = random(8);
+		pScreen[8 * y + x] |= 0x01;
+	}
+	cube.sendBatch(pScreen, length);
+}
+
+void callAnimation(void (*p)(), uint16_t span, uint16_t times, byte init_value,
+		Controller_A3D8::InputMode mode)
+{
+	cube.sendMode(mode);
+	cube.sendGlobal(init_value);
+	while (times--)
+	{
+		(*p)();
+		delay(span);
+	}
+}
+
+void callAnimationInModes(void (*p)(), uint16_t span, uint16_t times,
+		byte init_value)
+{
+	for (byte i = 0; i < 3; i++)
+		callAnimation(p, span, times, init_value,
+				(Controller_A3D8::InputMode) i);
 }
 
 void loop()
 {
-	lcd.putDM();
-//	dm.setMoveDirection(DotMatrix::BIT_IN_COL_NEGA);
-//	dm.move(true);
-//	dm.setMoveDirection(DotMatrix::BIT_IN_ROW_NEGA);
-//	dm.move(true);
-	delay(200);
+	callAnimation(animationFlash, 0xF0, 0x08, 0x00, Controller_A3D8::XYZ);
+	callAnimation(animationBreathe, 0x08, 0xff * 4, 0xff, Controller_A3D8::XYZ);
+	callAnimationInModes(animationFacetScan, 0x80, 0x08, 0x00);
+	callAnimationInModes(animationBlockScan, 0x80, 0x08, 0x00);
+	callAnimationInModes(animationRiseZ,0x40, 0x60, 0x00);
 }
+
