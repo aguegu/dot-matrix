f350ed7e32f3ad5b999c5e12b24427c47b9426ba
Weihong Guan <weihong.guan@gmail.com>
2012-07-09 21:22:21 +0800

    Add Basic Driver for 595+138 with SPI

---
+++ BasicDriver/Driver_595_138_SPI_Basic.cpp
@@ -0,0 +1,20 @@
+/*
+ * Driver595138SPIBasic.cpp
+ *
+ *  Created on: 2012-7-9
+ *      Author: Agu
+ */
+
+#include "Driver_595_138_SPI_Basic.h"
+
+Driver_595_138_SPI_Basic::Driver_595_138_SPI_Basic()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Driver_595_138_SPI_Basic::~Driver_595_138_SPI_Basic()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ BasicDriver/Driver_595_138_SPI_Basic.h
@@ -0,0 +1,18 @@
+/*
+ * Driver595138SPIBasic.h
+ *
+ *  Created on: 2012-7-9
+ *      Author: Agu
+ */
+
+#ifndef DRIVER_595_138_SPI_BASIC_H_
+#define DRIVER_595_138_SPI_BASIC_H_
+
+class Driver_595_138_SPI_Basic
+{
+public:
+	Driver_595_138_SPI_Basic();
+	virtual ~Driver_595_138_SPI_Basic();
+};
+
+#endif /* DRIVER595138SPIBASIC_H_ */
