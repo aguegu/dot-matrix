ee4fce815905a5ade0dac096c883465302a58733
Weihong Guan <weihong.guan@gmail.com>
2012-09-04 14:30:11 +0800

    figure out the layout of Dual595 48x12 screen

--- DotMatrixTest.cpp
+++ DotMatrixTest.cpp
@@ -2,14 +2,35 @@
 
 void setup()
 {
-	for (byte i=0; i<20;i++)
-	{	pinMode(i, OUTPUT);
-		digitalWrite(i, HIGH);
-	}
-	digitalWrite(5,LOW);
+	pinMode(5, OUTPUT);
+	pinMode(6, OUTPUT);
+	pinMode(7, OUTPUT);
+	pinMode(8, OUTPUT);
+	pinMode(9, OUTPUT);
+
+	digitalWrite(5, LOW);
+//	digitalWrite(8, LOW);
+
 }
 
 void loop()
 {
+	static byte i = 0;
+	digitalWrite(8, i % 24);
+	digitalWrite(9, HIGH);
+	digitalWrite(7, HIGH);
+	digitalWrite(7, LOW);
+
+	digitalWrite(6, HIGH);
+	digitalWrite(6, LOW);
+	delay(500);
+
+	i++;
+
+	if (i == 24)
+	{
+		i = 0;
+		delay(1500);
+	}
 
 }
