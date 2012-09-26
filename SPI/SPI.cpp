837a0d506294d0da0419b4fdaaa73018e4f1a7de
weihong.guan <weihong.guan@gmail.com>
2012-09-27 00:36:53 +0800

    update sample for Nokia5110

--- SPI/SPI.cpp
+++ SPI/SPI.cpp
@@ -13,49 +13,54 @@
 
 SPIClass SPI;
 
-void SPIClass::begin() {
-  // Set direction register for SCK and MOSI pin.
-  // MISO pin automatically overrides to INPUT.
-  // When the SS pin is set as OUTPUT, it can be used as
-  // a general purpose output port (it doesn't influence
-  // SPI operations).
+void SPIClass::begin()
+{
+	// Set direction register for SCK and MOSI pin.
+	// MISO pin automatically overrides to INPUT.
+	// When the SS pin is set as OUTPUT, it can be used as
+	// a general purpose output port (it doesn't influence
+	// SPI operations).
 
-  pinMode(SCK, OUTPUT);
-  pinMode(MOSI, OUTPUT);
-  pinMode(SS, OUTPUT);
-  
-  digitalWrite(SCK, LOW);
-  digitalWrite(MOSI, LOW);
-  digitalWrite(SS, HIGH);
+	pinMode(SCK, OUTPUT);
+	pinMode(MOSI, OUTPUT);
+	pinMode(SS, OUTPUT);
 
-  // Warning: if the SS pin ever becomes a LOW INPUT then SPI 
-  // automatically switches to Slave, so the data direction of 
-  // the SS pin MUST be kept as OUTPUT.
-  SPCR |= _BV(MSTR);
-  SPCR |= _BV(SPE);
+	digitalWrite(SCK, LOW);
+	digitalWrite(MOSI, LOW);
+	digitalWrite(SS, HIGH);
+
+	// Warning: if the SS pin ever becomes a LOW INPUT then SPI
+	// automatically switches to Slave, so the data direction of
+	// the SS pin MUST be kept as OUTPUT.
+	SPCR |= _BV(MSTR);
+	SPCR |= _BV(SPE);
 }
 
-void SPIClass::end() {
-  SPCR &= ~_BV(SPE);
+void SPIClass::end()
+{
+	SPCR &= ~_BV(SPE);
 }
 
 void SPIClass::setBitOrder(uint8_t bitOrder)
 {
-  if(bitOrder == LSBFIRST) {
-    SPCR |= _BV(DORD);
-  } else {
-    SPCR &= ~(_BV(DORD));
-  }
+	if (bitOrder == LSBFIRST)
+	{
+		SPCR |= _BV(DORD);
+	}
+	else
+	{
+		SPCR &= ~(_BV(DORD));
+	}
 }
 
 void SPIClass::setDataMode(uint8_t mode)
 {
-  SPCR = (SPCR & ~SPI_MODE_MASK) | mode;
+	SPCR = (SPCR & ~SPI_MODE_MASK) | mode;
 }
 
 void SPIClass::setClockDivider(uint8_t rate)
 {
-  SPCR = (SPCR & ~SPI_CLOCK_MASK) | (rate & SPI_CLOCK_MASK);
-  SPSR = (SPSR & ~SPI_2XCLOCK_MASK) | ((rate >> 2) & SPI_2XCLOCK_MASK);
+	SPCR = (SPCR & ~SPI_CLOCK_MASK) | (rate & SPI_CLOCK_MASK);
+	SPSR = (SPSR & ~SPI_2XCLOCK_MASK) | ((rate >> 2) & SPI_2XCLOCK_MASK);
 }
 
