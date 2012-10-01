51edd7b33a3427e2a3ac27cbfd345afd2121d3f4
weihong.guan <weihong.guan@gmail.com>
2012-10-01 13:02:22 +0800

    add pinReverse with ^ operater

--- BasicDriver/PinWrite.h
+++ BasicDriver/PinWrite.h
@@ -9,5 +9,5 @@
 #define pinSet(pin)	*portOutputRegister(digitalPinToPort(pin)) |= digitalPinToBitMask(pin)
 #define pinClear(pin)	*portOutputRegister(digitalPinToPort(pin)) &= ~digitalPinToBitMask(pin)
 #define pinWrite(pin, val) ( (val) ? pinSet(pin) : pinClear(pin))
-
+#define pinReverse(pin)	*portOutputRegister(digitalPinToPort(pin)) ^= digitalPinToBitMask(pin)
 #endif
