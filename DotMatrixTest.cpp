497baac6447c857f9e13743cf6c0b5a6e8122b3b
Weihong.Guan <weihong.Guan@gmail.com>
2012-10-22 11:13:57 +0800

    sending bitmap by serial for 12864

--- DotMatrixTest.cpp
+++ DotMatrixTest.cpp
@@ -1,40 +1,49 @@
 #include "DotMatrixTest.h"
+
 #include "DotMatrix.h"
-#include "DotPattern.h"
-#include "Driver_PCD8544.h"
+#include "Driver_ST7920.h"
 
-#include "Pattern.h"
+ST7920 dmd(A5, A4, A3, A2, A1, A0);
+DotMatrix dm = dmd.getDotMatrix();
 
-Driver_PCD8544 lcd(A5, A4, A3, A2, A1);
-DotMatrix dm = lcd.getDM();
-DotPattern dp(dm);
+volatile bool completed = false;
 
 void setup()
 {
-	pinMode(A0, OUTPUT);
-	digitalWrite(A0, HIGH);
-
-	lcd.init();
-
+	Serial.begin(115200);
 	dm.clear();
-
-	byte p[72];
-	memcpy_P(p, PATTERN_CHAR, 72);
-	dp.setPattern(p, 72, 3);
-	dp.postAt(0, 0);
-
-	memcpy_P(p, PATTERN_CHAR+72, 72);
-	dp.setPattern(p, 72, 3);
-	dp.postAt(8, 42);
-
+	dmd.putDM();
 }
 
 void loop()
 {
-	lcd.putDM();
-	dm.setMoveDirection(DotMatrix::BIT_IN_COL_NEGA);
-	dm.move(true);
-	dm.setMoveDirection(DotMatrix::BIT_IN_ROW_NEGA);
-	dm.move(true);
-	delay(200);
+	if (completed)
+	{
+		dmd.putDM();
+		completed = false;
+	}
+}
+
+void serialEvent()
+{
+	static int index = 0;
+	byte data = Serial.read();
+
+	if (index == 0 && data == 0xf3)
+	{
+		index = 1;
+	}
+	else if (index)
+	{
+		if (index >= 1024)
+		{
+			index = 0;
+			completed = true;
+		}
+		else
+		{
+			dm.setByte(index - 1, data);
+			index = index + 1;
+		}
+	}
 }
